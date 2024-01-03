5f6e014f323e09bc4527b8e210f9d8baa897827d
rollo <rollovitch.roll@yandex.ru>
2024-01-03 23:36:24 +0300

    + user page

--- src/main/java/com/example/adminservice/dto/Role.java
+++ src/main/java/com/example/adminservice/dto/Role.java
@@ -1,11 +1,5 @@
 package com.example.adminservice.dto;
 
 public enum Role {
-    ADMIN("ADMIN"), USER("USER");
-
-    private final String role;
-
-    Role(String role) {
-        this.role = role;
-    }
+    ROLE_ADMIN, ROLE_USER;
 }
